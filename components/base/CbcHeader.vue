4e506d1231a6fd23f8d1e134e9c27fdf01a071c8
Jewel Josef P. Seno <jjpseno@vauldex.com>
2025-01-15 16:58:26 +0800

    Update header

--- components/base/CbcHeader.vue
+++ components/base/CbcHeader.vue
@@ -1,7 +1,25 @@
 <template>
   <section
-    class="w-full h-22 bg-rich-black text-white"  
+    class="w-full h-24 pb-0.5 flex justify-between bg-rich-black text-white"  
   >
-    <h2>Header</h2>
+    <div
+      class="flex items-center justify-center px-16 bg-blue-500/50 border-red-500 border-2"
+    >
+      Header
+    </div>
+    <nav
+      class="flex *:flex *:items-center *:justify-center *:px-8 *:bg-orange-500/50 *:border-red-500 *:border-2"
+    >
+      <NuxtLink
+        to="/"
+      >
+        Home
+      </NuxtLink>
+      <NuxtLink
+        to="/me"
+      >
+        Profile
+      </NuxtLink>
+    </nav>
   </section>
 </template>
