b5181ec5743888d21cae442ab6fe6f79d6e0c203
Jewel Josef P. Seno <jjpseno@vauldex.com>
2025-01-17 17:00:25 +0800

    Added test image; Updated business list

--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,22 @@
       class="flex flex-col gap-2 h-dvh w-full bg-slate-500/50"
     >
       <li
-        class="w-full rounded-md outline outline-2 outline-red-900 bg-green-500/50"
+        class="w-full h-24 rounded-md p-2 outline outline-2 outline-red-900 bg-green-500/50"
         v-for="bus in businesses"
         :key="bus.id"
       >
-        {{ bus.companyName }}
+        <div
+          class="flex"
+        >
+          <NuxtImg
+            class="h-20 w-20" src="misc/test.jpg" width="40" height="40" 
+          />
+          <p
+            class="ml-2"
+          >
+            {{ bus.companyName }}
+          </p>
+        </div>
       </li>
     </ul>
   </div>
