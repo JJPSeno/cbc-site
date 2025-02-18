4a4a8862abd52fd2080ccde84bd0c6f38a071e8c
Jewel Josef P. Seno <jjpseno@vauldex.com>
2025-02-18 09:48:20 +0800

    Update business profile

--- components/business/CbcBusinessProfile.vue
+++ components/business/CbcBusinessProfile.vue
@@ -1,8 +1,47 @@
 <template>
   <div
-    class="w-full min-h-screen rounded-md p-2 shadow-xl bg-white"
+    class="w-full min-h-screen rounded-md p-2 shadow-xl bg-white flex"
   >
-    {{ businessProfile?.companyName }}
+    <div>
+      <NuxtImg
+        class="border-2 border-black aspect-square" src="misc/test.jpg" width="250" height="250"
+        preload
+      />
+    </div>
+    <div
+      class="flex flex-col"
+    >
+      <p
+        class="p-2"
+      >
+        Company name: {{ businessProfile?.companyName }}
+      </p>
+      <p
+        class="p-2"
+      >
+        Address: {{ businessProfile?.address }}
+      </p>
+      <p
+        class="p-2"
+      >
+        Contact Info:
+      </p>
+      <div
+        class="ml-12"
+      >
+        <p
+          class="p-2"
+        >
+          {{ businessProfile?.contactInfo.phoneNumber }}
+        </p>
+        <p
+          v-if="businessProfile?.contactInfo.email"
+          class="p-2"
+        >
+          {{ businessProfile?.contactInfo.email }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -10,6 +49,7 @@ const route = useRoute()
 const { businesses } = useBusiness()
 const businessProfile = ref(businesses.value.find((b) => b.id == route.params.id))
 onMounted(() => {
+  console.log(businessProfile.value)
   if (!businessProfile.value) {
     return navigateTo('/business')
   }
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
     <CbcHeader />
     <main
-      class="min-h-dvh pt-32 sm:pt-44  pb-4 sm:pb-20 px-4 sm:px-40 bg-indigo-300"
+      class="min-h-dvh pt-28 sm:pt-32  pb-4 sm:pb-20 px-4 sm:px-40 bg-indigo-300"
     >
       <slot />
     </main>
